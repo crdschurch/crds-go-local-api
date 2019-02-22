45f2887ec207bf008c7daa8326b2b60681ac4732
vchtel <VictorChtelmakh@gmail.com>
2019-02-22 16:08:50 -0500

    Add logging, a service to fetch environment variables, and swagger.

--- CrdsGoLocalApi/CrdsGoLocalApi/Program.cs
+++ CrdsGoLocalApi/CrdsGoLocalApi/Program.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Logzio.DotNet.NLog;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
+using NLog.Web;
+using System;
 
 namespace CrdsGoLocalApi
 {
@@ -20,5 +19,64 @@ namespace CrdsGoLocalApi
     public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
         WebHost.CreateDefaultBuilder(args)
             .UseStartup<Startup>();
+
+    static NLog.Logger SetUpLogging()
+    {
+
+      //TODO: Load this from a singleton of SettingsService
+      var isDevelopment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == EnvironmentName.Development;
+
+      var loggingConfig = new LoggingConfiguration();
+
+      //Set up to log to stdout
+      if (isDevelopment)
+      {
+        var consoleTarget = new ColoredConsoleTarget("console")
+        {
+          Layout = @"${date:format=HH\:mm\:ss} ${level} ${message} ${exception:format=ToString}"
+        };
+        loggingConfig.AddTarget("console", consoleTarget);
+
+        //Log everything in development
+        loggingConfig.AddRuleForAllLevels(consoleTarget, "*");
+      }
+      else // Log to Logzio
+      {
+        var logzioTarget = new LogzioTarget
+        {
+          //TODO: Load this from a singleton of SettingsService
+          Token = Environment.GetEnvironmentVariable("LOGZ_IO_KEY"),
+        };
+        logzioTarget.ContextProperties.Add(new TargetPropertyWithContext("host", "${machinename}"));
+        logzioTarget.ContextProperties.Add(new TargetPropertyWithContext("application", Environment.GetEnvironmentVariable("APP_NAME")));
+        logzioTarget.ContextProperties.Add(new TargetPropertyWithContext("environment", Environment.GetEnvironmentVariable("CRDS_ENV")));
+        loggingConfig.AddTarget("logzio", logzioTarget);
+
+        //Log only error and above for all built in logs
+        loggingConfig.AddRule(NLog.LogLevel.Error, NLog.LogLevel.Fatal, logzioTarget, "*");
+
+        //Log everything debug and above for custom logs
+        loggingConfig.AddRule(NLog.LogLevel.Debug, NLog.LogLevel.Fatal, logzioTarget, "Crossroads.*");
+
+        // Also log to console so we have the info
+        var consoleTarget = new ColoredConsoleTarget("console")
+        {
+          Layout = @"${date:format=HH\:mm\:ss} ${level} ${message} ${exception:format=ToString}"
+        };
+        loggingConfig.AddTarget("console", consoleTarget);
+
+        //Log only warn and above for all built in logs
+        loggingConfig.AddRule(NLog.LogLevel.Error, NLog.LogLevel.Fatal, consoleTarget, "*");
+
+        //Log everything debug and above for custom logs
+        loggingConfig.AddRule(NLog.LogLevel.Debug, NLog.LogLevel.Fatal, consoleTarget, "Crossroads.*");
+
+        var databaseTarget = new DatabaseTarget();
+      }
+
+      LogManager.Configuration = loggingConfig;
+
+      return NLogBuilder.ConfigureNLog(loggingConfig).GetCurrentClassLogger();
+    }
   }
 }
--- CrdsGoLocalApi/CrdsGoLocalApi/Startup.cs
+++ CrdsGoLocalApi/CrdsGoLocalApi/Startup.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CrdsGoLocalApi.Services;
+using CrdsGoLocalApi.Services.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -31,6 +33,8 @@ namespace CrdsGoLocalApi
       services.AddSwaggerGen(c => {
         c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
       });
+
+      services.AddSingleton<ISettingsService, SettingsService>();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
