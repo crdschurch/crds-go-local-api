7c5c1a0ca81bdce52dea9c8e0454b6784b1315ed
andycanterbury <andrew.canterbury@ingagepartners.com>
2019-03-10 21:25:08 -0400

    US16545 first pass at Volunteer DTO

---
+++ CrdsGoLocalApi/Models/VolunteerDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CrdsGoLocalApi.Models
+{
+  public class VolunteerDTO
+  {
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public DateTime BirthDate { get; set; }
+  }
+}
