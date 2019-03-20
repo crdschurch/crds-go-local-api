95688decd6f69685b73e04ad945fa48d2ff68de7
andycanterbury <andrew.canterbury@ingagepartners.com>
2019-03-20 11:27:35 -0400

    US16505 semi colons!

--- CI/SQL/20190320_093326_US16505-go-local-security-for-confirmation-email.sql
+++ CI/SQL/20190320_093326_US16505-go-local-security-for-confirmation-email.sql
@@ -1,8 +1,8 @@
 USE MinistryPlatform
 GO 
 
-DECLARE @GoLocalRoleId int = 211
-DECLARE @GroupPageId int = 322
+DECLARE @GoLocalRoleId int = 211;
+DECLARE @GroupPageId int = 322;
 
 IF NOT EXISTS (SELECT 1 FROM dp_Role_Pages WHERE Role_ID = @GoLocalRoleId AND Page_ID = @GroupPageId)
 BEGIN 
