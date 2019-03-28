c195729b9140e67b5b7318155315ba7ab4b0e568
vchtel <VictorChtelmakh@gmail.com>
2019-03-28 11:42:15 -0400

    Andy hates using statements.

--- CrdsGoLocalApi/Constants/ProjectStatusIds.cs
+++ CrdsGoLocalApi/Constants/ProjectStatusIds.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace CrdsGoLocalApi.Constants
+﻿namespace CrdsGoLocalApi.Constants
 {
   public static class ProjectStatusIds
   {
