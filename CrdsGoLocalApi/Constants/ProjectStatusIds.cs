8cd8a3d0c435ab3baac2ee2cab09878c80f3780b
vchtel <VictorChtelmakh@gmail.com>
2019-03-28 11:57:39 -0400

    Fixed the thing

--- CrdsGoLocalApi/Constants/ProjectStatusIds.cs
+++ CrdsGoLocalApi/Constants/ProjectStatusIds.cs
@@ -3,8 +3,6 @@
   public static class ProjectStatusIds
   {
     public static int ACTIVE = 1;
-    public static int CANCELLED = 2;
     public static int CLOSED_FOR_SIGNUPS = 3;
-    public static int PAUSED = 4;
   }
 }
